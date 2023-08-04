03eaa9c2290ba5c1f572b8f2d3f7219bc599cf05
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-08-04 18:35:14 -0300

    Customizing the interceptor's DTO

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -7,15 +7,16 @@ import {
 
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { plainToClass } from 'class-transformer';
-import { UserDto } from 'src/users/dtos/user.dto';
+import { plainToInstance } from 'class-transformer';
 
 export class SerializeInterceptor implements NestInterceptor {
+    constructor(private dto: any) {};
+
     intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
 
         return handler.handle().pipe(
             map((data: any) => {
-                return plainToClass(UserDto, data, {
+                return plainToInstance(this.dto, data, {
                     excludeExtraneousValues: true,
                 });
             })
