5c4c04ac8bcc41aeefe5d65c5a272c4fa3bdb798
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-08-07 15:49:10 -0300

    Creating Authentication service logic and controller

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,10 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
+import { Script } from 'vm';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -9,25 +12,41 @@ export class AuthService {
 
     async signup(email: string, password: string) {
         //See if email is in use
-        const user = await this.usersServices.find(email);
+        const users = await this.usersServices.find(email);
 
-        if (user.length) {
+        if (users.length) {
             throw new BadRequestException('Email in use');
         };
         // Hash the users password
         // Generet a salt
+        const salt = randomBytes(8).toString('hex');
 
         // hash the salt and the password together
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
 
         // Join the hashed result and the salt together
+        const result = salt + '.' + hash.toString('hex');
 
         // Create a new user and save it
+        const user = await this.usersServices.create(email, result);
 
         //return the user
+        return user;
     };
 
-    signin() {
+    async signin(email: string, password: string) {
+        const [user] = await this.usersServices.find(email);
 
+        if (!user) throw new NotFoundException('User not found');
+
+        const [salt, storedHash] = user.password.split('.');
+
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+        if (storedHash !== hash.toString('hex')) throw new BadRequestException('Bad password');
+
+        return user;
     };
+
 
 };
