35980eb005ad2f203dd33a1ea117e7a8ed005e50
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-08-07 16:41:39 -0300

    Signing Out a User

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -37,6 +37,11 @@ export class UsersController {
         return user;
     };
 
+    @Post('/signout')
+    signOut(@Session() session: any) {
+        session.userId = null;
+    };
+
     @Post('/signin')
     async signin(@Body() body: CreateUserDto, @Session() session: any) {
         const user = await this.authService.signin(body.email, body.password);
